f067d0e01b8be1f25c168c54dd5eb041b5beefd1
MatiasViecho <mpviecho@gmail.com>
2024-07-29 17:17:00 -0300

    feat: add gsap and animations for elements

--- app/ui/main-card.tsx
+++ app/ui/main-card.tsx
@@ -1,8 +1,67 @@
+"use client";
 import Image from "next/image";
-
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+gsap.registerPlugin(useGSAP);
 export default function MainCard() {
+  const container = useRef<HTMLDivElement>(null);
+  useGSAP(
+    () => {
+      const tl = gsap.timeline();
+
+      tl.from(container.current, {
+        duration: 0.8,
+        opacity: 0,
+        translateY: -50,
+      })
+        .to(container.current, {
+          duration: 0.8,
+          opacity: 1,
+          translateY: 0,
+        })
+        .from(
+          ".text-animated",
+          {
+            stagger: 0.05,
+            translateY: +100,
+            opacity: 0,
+          },
+          "-=1.2"
+        )
+        .to(
+          ".text-animated",
+          {
+            translateY: 0,
+            opacity: 1,
+            stagger: 0.1,
+          },
+          "-=1.2"
+        )
+        .from(
+          ".link-icon",
+          {
+            stagger: 0.05,
+            translateY: +100,
+          },
+          "-=1.2"
+        )
+        .to(
+          ".link-icon",
+          {
+            translateY: 0,
+            stagger: 0.1,
+          },
+          "-=1.2"
+        );
+    },
+    { scope: container }
+  );
   return (
-    <div className="flex flex-col w-[250px] h-[350px] gap-3 p-2  border-slate border-4 rounded-2xl bg-cloud/70 backdrop-blur-[2px]">
+    <div
+      ref={container}
+      className="overflow-hidden flex flex-col w-[250px] h-[350px] gap-3 p-2  border-slate border-4 rounded-2xl bg-cloud/70 backdrop-blur-[2px]"
+    >
       <Image
         alt="portrait"
         src={"/mock-photo.png"}
@@ -11,34 +70,38 @@ export default function MainCard() {
         height={150}
       />
       <div className="gap-2 flex flex-col w-full">
-        <span className="font-xanh_mono text-iris text-[40px] tracking-tight">
+        <span className="font-xanh_mono text-iris text-[40px] tracking-tight text-animated">
           I&apos;m Matias
         </span>
-        <div className="flex flex-col gap-0 font-semibold font-work_sans text-sm">
+        <div className="flex flex-col gap-0 font-semibold font-work_sans text-sm text-animated">
           <span>Fullstack web developer</span>
           <span> +1 year of experience + studies</span>
         </div>
       </div>
       <div className="flex gap-2 max-h-10">
         <Image
+          className="link-icon"
           alt="linkedin icon"
           width={40}
           height={40}
           src="/icons/icon-linkedin.png"
         />
         <Image
+          className="link-icon"
           alt="github icon"
           width={40}
           height={40}
           src="/icons/icon-github.png"
         />
         <Image
+          className="link-icon"
           alt="email icon"
           width={60}
           height={40}
           src="/icons/icon-mail.png"
         />
         <Image
+          className="link-icon"
           alt="resume icon"
           width={30}
           height={40}
--- app/ui/presentation-text.tsx
+++ app/ui/presentation-text.tsx
@@ -1,6 +1,35 @@
+"use client";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/src/ScrollTrigger";
+import { useRef } from "react";
+gsap.registerPlugin(ScrollTrigger, useGSAP);
 export default function PresentationText() {
+  const ref = useRef<HTMLDivElement>(null);
+  useGSAP(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ref.current,
+        start: "top 80%",
+        end: "bottom center",
+        scrub: false,
+        markers: true,
+        toggleActions: "play reverse reverse complete",
+      },
+    });
+    tl.from(ref.current, {
+      translateY: +100,
+      opacity: 0,
+    }).to(ref.current, {
+      translateY: 0,
+      opacity: 1,
+    });
+  });
   return (
-    <div className="flex flex-col gap-8 px-2 font-medium font-work_sans text-sm">
+    <div
+      className="flex flex-col gap-8 px-2 font-medium font-work_sans text-sm"
+      ref={ref}
+    >
       <div>
         <span>I&apos;m a 23 yo. web developer from Argentina.</span>
         <p>
