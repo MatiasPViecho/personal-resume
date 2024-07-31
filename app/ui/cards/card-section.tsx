cc4bd212e574e6838aa1587a9cf1ad2a7b1802e8
MatiasViecho <mpviecho@gmail.com>
2024-07-31 12:11:14 -0300

    feat: add cards section

--- app/ui/cards/card-section.tsx
+++ app/ui/cards/card-section.tsx
@@ -1,3 +1,13 @@
+import Card from "./card";
+
 export default function CardSection() {
-  return <div>tst</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Card name="Next.js" imgPath="/tech/next-js.svg" alt="Next.JS Icon" />
+        </li>
+      </ul>
+    </div>
+  );
 }
---
+++ app/ui/cards/card.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+export default function Card({
+  imgPath,
+  alt,
+  name,
+}: {
+  imgPath: string;
+  alt: string;
+  name: string;
+}) {
+  return (
+    <div className="bg-cloud content-center border-onyx rounded-lg border w-[100px] h-[125px] md:w-[200px] md:h-[250px] py-4 px-3 text-sm md:text-lg">
+      <Image
+        src={imgPath}
+        alt={alt}
+        width={128}
+        height={128}
+        className={"md:w-32 md:h-32 w-16 h-16 mx-auto"}
+      />
+      <p className="font-work_sans font-semibold pt-8 select-none text-sm text-center">
+        {name}
+      </p>
+    </div>
+  );
+}
